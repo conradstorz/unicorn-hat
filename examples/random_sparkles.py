cbe6d8958956bd71c0c273584deefa05fab8ba78
Conrad <conradstorz@gmail.com>
2020-03-15 23:41:09 -0400

    changed type sense from AUTO to HAT

--- examples/random_sparkles.py
+++ examples/random_sparkles.py
@@ -13,7 +13,7 @@ If you're using a Unicorn HAT and only half the screen lights up,
 edit this example and  change 'unicorn.AUTO' to 'unicorn.HAT' below.
 """)
 
-unicorn.set_layout(unicorn.AUTO)
+unicorn.set_layout(unicorn.HAT)
 unicorn.rotation(0)
 unicorn.brightness(0.5)
 #width,height=unicorn.get_shape()
